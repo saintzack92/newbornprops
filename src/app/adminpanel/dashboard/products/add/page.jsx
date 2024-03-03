54653de94a28fd8ffd7469db0ca7d0767becebdc
Abdullah Chaniago <abdullah.chaniago92@gmail.com>
2024-03-03 17:29:07 +0700

    gas

--- src/app/adminpanel/dashboard/products/add/page.jsx
+++ src/app/adminpanel/dashboard/products/add/page.jsx
@@ -119,9 +119,9 @@ const AddProductPage = () => {
           className={`${styles.formChild} ${styles.formChildInput}`}
           
         />
-        <div className={`w-full gap-[20px] flex flex-col rounded-[5px] py-[80px] mb-[20px] bg-[var(--bg)] justify-center text-center items-center `}>
+        <div className={`w-full gap-[20px] flex flex-col rounded-[5px] py-[80px] mb-[20px] bg-[var(--bg)] justify-center text-center items-center border-solid border-[#2e374a] border-2`}>
           <h1 className="">input your article here</h1>
-          <ReactQuil className={` h-[550px] px-[30px] mb-[20px] w-full`} onChange={handleContentChange} />
+          <ReactQuil className={` h-[550px] px-[30px] mb-[20px] w-full `} onChange={handleContentChange} />
         </div>
 
         <button type="submit" className={` w-[100%] p-[30px] !bg-[teal] !text-[var(--text)] rounded-[5px] cursor-pointer ${styles.formChild}`}>
