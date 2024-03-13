7f739561c8e9e22dab34578bccdfe86433a1838a
Abdullah Chaniago <abdullah.chaniago@ai.astra.co.id>
2024-03-13 11:34:02 +0700

    add button x

--- src/app/adminpanel/dashboard/articles/add/page.jsx
+++ src/app/adminpanel/dashboard/articles/add/page.jsx
@@ -2,7 +2,7 @@
 import styles from "../../../ui/dashboard/products/addProduct/addProduct.module.css";
 import img from "../../../../../../public/7.jpg";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { ReactQuil } from "../../../ui/dashboard/component/quill"; // Adjust the path as necessary
 
 const AddProductPage = () => {
@@ -17,6 +17,7 @@ const AddProductPage = () => {
     content: { html: "" },
   });
   const [imagePreviewUrl, setImagePreviewUrl] = useState("");
+  const fileInputRef = useRef(null); // Using useRef for the file input
 
   const handleImageChange = (e) => {
     e.preventDefault();
@@ -33,6 +34,11 @@ const AddProductPage = () => {
     };
 
     reader.readAsDataURL(file);
+  };
+  const handleRemoveImage = () => {
+    setImagePreviewUrl(''); // Use setImagePreviewUrl to update state
+    // Reset the file input
+    if (fileInputRef.current) fileInputRef.current.value = '';
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -203,17 +209,25 @@ const AddProductPage = () => {
         <div
           className={`w-full gap-[20px] flex flex-col rounded-[5px] py-[80px] mb-[20px] bg-[var(--bg)] justify-center text-center items-center border-solid border-[#2e374a] border-2`}
         >
+           
           <div
-            className={`w-[95%] px-[20px] gap-[20px] flex flex-col rounded-[5px] py-[80px] mb-[20px] bg-[var(--bg)] justify-center text-center items-center border-solid border-[#2e374a] border-2 relative z-0 h-[400px]`}
+            className={`w-[95%] gap-[20px] flex flex-col rounded-[5px]  mb-[20px] bg-[var(--bg)] justify-center text-center items-center border-solid border-[#2e374a] border-2 relative z-0 h-[400px]`}
           >
             {imagePreviewUrl && (
-              <img
-                src={imagePreviewUrl}
-                alt="Image Preview"
-                style={{ width: "100%" }} // Adjust styling as needed
-                className={`z-10 flex absolute object-contain h-full`}
-              />
-            )}
+          // <div className="flex  bg-[blue] w-full">
+          <div className={`w-[100%] h-full px-[20px] gap-[20px] relative flex-col rounded-[5px]  bg-[var(--bg)] justify-center text-center items-center border-none inline-block `}
+          >
+            <button onClick={handleRemoveImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline right-2 z-50 absolute  ">X</button> 
+            <img
+              src={imagePreviewUrl}
+              alt="Image Preview"
+              style={{ width: "100%" }} // Adjust styling as needed
+              className={`z-10 flex absolute object-contain h-full`}
+            />
+          </div>
+            
+            
+        )}
 
             {!imagePreviewUrl && "Upload your main image here"}
             <input
