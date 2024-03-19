5e76b5a4a843e80ec421496a95ebfdba51f51c30
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2024-03-19 15:32:34 +0700

    wed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 // import { NextResponse } from "next/server";
 
-
 // function getCookieValue(cookieString:any, cookieName:any) {
 //   const name = cookieName + "=";
 //   const decodedCookie = decodeURIComponent(cookieString);
@@ -23,9 +22,7 @@
 //   // Extract the access_token from the cookieHeader
 //   const accessToken = getCookieValue(cookieHeader, 'access_token');
 //   // console.log(accessToken);
-  
-  
-  
+
 //   // Check if access_token is not empty
 //   if (accessToken) {
 //       // Make a request to the backend validation endpoint with the Authorization header
@@ -54,10 +51,10 @@
 import { NextResponse } from "next/server";
 import { setToken } from "./app/lib/authToken";
 
-function getCookieValue(cookieString:any, cookieName:any) {
+function getCookieValue(cookieString: any, cookieName: any) {
   const name = cookieName + "=";
   const decodedCookie = decodeURIComponent(cookieString);
-  const ca = decodedCookie.split(';');
+  const ca = decodedCookie.split(";");
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i].trim();
     if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
@@ -65,40 +62,49 @@ function getCookieValue(cookieString:any, cookieName:any) {
   return "";
 }
 
-async function refreshToken(request:any) {
-  const refreshToken = getCookieValue(request.headers.get('cookie'), 'refresh_token');
+async function refreshToken(request: any) {
+  const refreshToken = getCookieValue(
+    request.headers.get("cookie"),
+    "refresh_token"
+  );
   if (!refreshToken) return null;
 
   const response = await fetch(`http://localhost:3000/auth/refresh`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ refresh_token: refreshToken }),
   });
-  console.log(JSON.stringify(response,null,4));
-  console.log(refreshToken,'refresh token function in middleware');
-  
+  console.log(JSON.stringify(response, null, 4));
+  console.log(refreshToken, "refresh token function in middleware");
 
   if (!response.ok) return null;
   return await response.json();
 }
 
-export async function middleware(request:any) {
-  const accessToken = getCookieValue(request.headers.get('cookie'), 'access_token');
+export async function middleware(request: any) {
+  const accessToken = getCookieValue(
+    request.headers.get("cookie"),
+    "access_token"
+  );
   const url = request.nextUrl.clone(); // Clone the URL to modify it
-  
-  if (!accessToken && !url.pathname.startsWith('/login')) {
-    url.pathname = '/login';
+
+  // Redirect to login page if token is not available and user is not on the login page
+  if (!accessToken && !url.pathname.startsWith("/login")) {
+    url.pathname = "/login";
     return NextResponse.redirect(url);
   }
-  
+
   if (accessToken) {
-    const validationResponse = await fetch(`http://localhost:3000/auth/status`, {
-      headers: { 'Authorization': `Bearer ${accessToken}` },
-    });
-    
+    const validationResponse = await fetch(
+      `http://localhost:3000/auth/status`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+
     if (validationResponse.ok) {
-      if (url.pathname === '/login') {
-        url.pathname = '/adminpanel/dashboard';
+      if (url.pathname === "/login") {
+        url.pathname = "/adminpanel/dashboard";
         return NextResponse.redirect(url);
       }
       return NextResponse.next();
@@ -106,20 +112,32 @@ export async function middleware(request:any) {
       const newTokens = await refreshToken(request);
       if (newTokens && newTokens.access_token) {
         const response = NextResponse.next();
-        response.cookies.set('access_token', newTokens.access_token, { path: '/', httpOnly: true, sameSite: 'strict' });
-        setToken(newTokens.data.loginResponse.access_token)
-        if (newTokens.refresh_token) response.cookies.set('refresh_token', newTokens.refresh_token, { path: '/', httpOnly: true, sameSite: 'strict' });
+        response.cookies.set("access_token", newTokens.access_token, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+        });
+        setToken(newTokens.data.loginResponse.access_token);
+        
+        if (newTokens.refresh_token)
+          response.cookies.set("refresh_token", newTokens.refresh_token, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+          });
         return response; // Proceed with new tokens set
       }
     }
   }
 
+ 
+
   // Redirect to login if not accessing login page and no valid access token
-  if (!url.pathname.startsWith('/login')) {
-    url.pathname = '/login';
+  if (!url.pathname.startsWith("/login")) {
+    url.pathname = "/login";
     return NextResponse.redirect(url);
   }
-  
+
   return NextResponse.next();
 }
 
