2bb364ad67cfb250a4e1acb6f03b7053b4d56a12
Rizsky Darmawan <darmawanrizsky@gmail.com>
2024-03-21 23:12:43 +0700

    jika data tidak ada

--- src/app/adminpanel/dashboard/articles/page.jsx
+++ src/app/adminpanel/dashboard/articles/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import styles from "../../../adminpanel/ui/dashboard/products/products.module.css";
 import Search from "../../ui/dashboard/search/search";
 import Link from "next/link";
@@ -6,11 +6,10 @@ import Pagination from "../../ui/dashboard/pagination/pagination";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import CardTable from "../../ui/dashboard/card/cardTabel";
-  
 
-const ProductsPage = ()=>{
-  const router = useRouter()
-  
+const ProductsPage = () => {
+  const router = useRouter();
+
   const [formData, setFormData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
@@ -19,9 +18,12 @@ const ProductsPage = ()=>{
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/article/all?page=${currentPage}&limit=${itemsPerPage}`, {
-          method: "GET",
-        });
+        const response = await fetch(
+          `http://localhost:3000/article/all?page=${currentPage}&limit=${itemsPerPage}`,
+          {
+            method: "GET",
+          }
+        );
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -32,8 +34,8 @@ const ProductsPage = ()=>{
       } catch (error) {
         console.error("Failed to fetch data:", error);
         // Consider redirecting to login if unauthorized
-        if (error.message.includes('401')) {
-          router.push('/login');
+        if (error.message.includes("401")) {
+          router.push("/login");
         }
       }
     };
@@ -41,21 +43,23 @@ const ProductsPage = ()=>{
     fetchData();
   }, [currentPage, router]);
   const handlePrevPage = () => {
-    setCurrentPage(prevPage => Math.max(1, prevPage - 1));
+    setCurrentPage((prevPage) => Math.max(1, prevPage - 1));
   };
 
   const handleNextPage = () => {
-    setCurrentPage(prevPage => Math.min(totalPages, prevPage + 1));
+    setCurrentPage((prevPage) => Math.min(totalPages, prevPage + 1));
   };
   const handleViewProduct = (slug) => {
     router.push(`articles/${slug}`); // Navigate to the single product page
   };
 
-    return (
-        <div
+  return (
+    <div
       className={`${styles.container} p-[20px] bg-[var(--bgSoft)] rounded-[10px] mt-[20px]`}
     >
-      <div className={`${styles.top} flex items-center justify-between py-[10px]`}>
+      <div
+        className={`${styles.top} flex items-center justify-between py-[10px]`}
+      >
         <Search placeholder="Search for a user" />
         <Link href={"articles/add"}>
           <button
@@ -77,31 +81,38 @@ const ProductsPage = ()=>{
             <td>Action</td>
           </tr>
         </thead>
-        <tbody >
-        {formData.map((formDatas) => (
-          
-            <CardTable
-              key={formDatas.id}
-              title={formDatas.title}
-              category={formDatas.category}
-              description={formDatas.description}
-              isActive={formDatas.active}
-              isHighlights={formDatas.highlights}
-              slug={formDatas.slug} // Ensure this is correctly passed
-
-            />
-            
-          ))}
+        <tbody>
+          {formData.length > 0 ? (
+            formData.map((formDatas) => (
+              <CardTable
+                key={formDatas.id}
+                title={formDatas.title}
+                category={formDatas.category}
+                description={formDatas.description}
+                isActive={formDatas.active}
+                isHighlights={formDatas.highlights}
+                slug={formDatas.slug} // Ensure this is correctly passed
+              />
+            ))
+          ) : (
+            <tr>
+              <th colspan="12">
+                <div className="text-center m-3 p-3">
+                  - Data tidak ditemukan -
+                </div>
+              </th>
+            </tr>
+          )}
         </tbody>
       </table>
-      <Pagination 
-      nextFunc={handleNextPage}
-      prevPage={handlePrevPage}
-      currentPage={currentPage}
-      totalPages={totalPages}
+      <Pagination
+        nextFunc={handleNextPage}
+        prevPage={handlePrevPage}
+        currentPage={currentPage}
+        totalPages={totalPages}
       />
     </div>
-    )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
